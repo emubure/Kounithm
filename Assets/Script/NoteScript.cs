37751f8ef1c90ad04597dabad4fbcc58488da74c
emubure <emb.20915@gmail.com>
2017-06-11 10:58:20 +0900

    メッシュの移動速度が遅い

--- Assets/Script/NoteScript.cs
+++ Assets/Script/NoteScript.cs
@@ -9,17 +9,20 @@ public class NoteScript : MonoBehaviour {
 
 	private AudioSource tapSound;
 
-	float noteZ = 0;
+	public float noteZ = 0;
 	bool NoteJudgeLinePassed = false;
 
 	//	----hold用----
 	public bool HoldTrue = false;
 	public int _noteOffsetf = 0;
-	float noteZf = 0;
+	public float noteZf = 0;
 
 
 	void Start(){
 		tapSound = GameObject.Find ("NoteSound").GetComponent<AudioSource>();
+		if (HoldTrue == true) {
+			createHoldMesh ();
+		}
 	}
 
 	void Update(){
@@ -30,12 +33,12 @@ public class NoteScript : MonoBehaviour {
 		noteZf = 0 + 1 * ((_noteOffsetf - GameManager.MusicOffset) * 20 * 0.003f);
 
 		if (HoldTrue == false) {
-			createTap ();
+			updateTap ();
 		} else if (HoldTrue == true) {
-			createHoldMesh ();
+			updateHoldMesh ();
 		}
 	}
-	void createTap(){
+	void updateTap(){
 		this.transform.position = new Vector3 (_lineNum, 0, noteZ);
 
 		//判定ラインに来たら破壊
@@ -44,7 +47,7 @@ public class NoteScript : MonoBehaviour {
 			tapSound.PlayOneShot (tapSound.clip);
 		}
 		if (this.transform.position.z <= -2f) {
-			Destroy (gameObject);
+			//Destroy (gameObject);
 		}
 	}
 
@@ -55,10 +58,10 @@ public class NoteScript : MonoBehaviour {
 
 			var mesh = new Mesh ();
 			mesh.vertices = new Vector3[] {
-				new Vector3 (0, 0.1f, noteZ),//左下
-				new Vector3 (1, 0.1f, noteZ),//右下
-				new Vector3 (1, 0.1f, noteZf),//右上
-				new Vector3 (0, 0.1f, noteZf),//左上
+			new Vector3 (_lineNum-0.5f, 0.1f, noteZ),//左下
+			new Vector3 (_lineNum+0.5f, 0.1f, noteZ),//右下
+			new Vector3 (_lineNum+0.5f, 0.1f, noteZf),//右上
+			new Vector3 (_lineNum-0.5f, 0.1f, noteZf),//左上
 			};
 
 			/*mesh.uv = new Vector2[] {
@@ -78,5 +81,27 @@ public class NoteScript : MonoBehaviour {
 
 			var filter = GetComponent<MeshFilter> ();
 			filter.sharedMesh = mesh;
+	}
+	void updateHoldMesh(){
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		Vector3[] vertices = mesh.vertices;
+
+		vertices [0].x = _lineNum - 0.5f;
+		vertices [1].x = _lineNum + 0.5f;
+		vertices [2].x = _lineNum + 0.5f;
+		vertices [3].x = _lineNum - 0.5f;
+
+		vertices [0].y = 0.1f;
+		vertices [1].y = 0.1f;
+		vertices [2].y = 0.1f;
+		vertices [3].y = 0.1f;
+
+		vertices [0].z = noteZ;
+		vertices [1].z = noteZ;
+		vertices [2].z = noteZf;
+		vertices [3].z = noteZf;
+
+		mesh.vertices = vertices;
+		mesh.RecalculateBounds();
 	}
 }
